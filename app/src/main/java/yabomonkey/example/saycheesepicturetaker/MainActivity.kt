e4072f2e00243b53b7f7d5032ccc814a96c67112
Dan <yabomonkey@gmail.com>
2022-03-25 00:43:10 -0700

    Removed Fragments for simplicity

--- app/src/main/java/yabomonkey/example/saycheesepicturetaker/MainActivity.kt
+++ app/src/main/java/yabomonkey/example/saycheesepicturetaker/MainActivity.kt
@@ -1,14 +1,14 @@
 package yabomonkey.example.saycheesepicturetaker
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import yabomonkey.example.saycheesepicturetaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
