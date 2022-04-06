83272f8bafb884aea2fe3477132ce153629557f4
Dan <yabomonkey@gmail.com>
2022-04-06 14:56:32 -0700

    Add OpenGalleryActivity and start fleshing out funs

--- app/src/main/java/yabomonkey/example/saycheesepicturetaker/OpenGalleryActivity.kt
+++ app/src/main/java/yabomonkey/example/saycheesepicturetaker/OpenGalleryActivity.kt
@@ -1,4 +1,22 @@
 package yabomonkey.example.saycheesepicturetaker
 
-class OpenGalleryActivity {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import yabomonkey.example.saycheesepicturetaker.databinding.ActivityGalleryBinding
+
+private const val TAG = "OpenGalleryActivity"
+
+class OpenGalleryActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityGalleryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        Log.d(TAG, "onCreate called")
+    }
+
+
 }
