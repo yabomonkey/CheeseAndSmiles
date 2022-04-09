fb5903503da219ad18aef6021f6ec3701557a700
Dan <yabomonkey@gmail.com>
2022-04-08 21:48:20 -0700

    Added utility functions to ViewExtensions.kt

--- app/src/main/java/yabomonkey/example/saycheesepicturetaker/utils/ViewExtensions.kt
+++ app/src/main/java/yabomonkey/example/saycheesepicturetaker/utils/ViewExtensions.kt
@@ -19,7 +19,18 @@ package yabomonkey.example.saycheesepicturetaker.utils
 import android.os.Build
 import android.view.DisplayCutout
 import android.view.View
+import android.view.WindowManager
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+
+/** Combination of all flags required to put activity into immersive mode */
+const val FLAGS_FULLSCREEN =
+    View.SYSTEM_UI_FLAG_LOW_PROFILE or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
 /** Pad this view with the insets provided by the device cutout (i.e. notch) */
 @RequiresApi(Build.VERSION_CODES.P)
@@ -40,5 +51,22 @@ fun View.padWithDisplayCutout() {
         insets.displayCutout?.let { doPadding(it) }
         insets
     }
+}
+
+/** Same as [AlertDialog.show] but setting immersive mode in the dialog's window */
+fun AlertDialog.showImmersive() {
+    // Set the dialog to not focusable
+    window?.setFlags(
+        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+
+    // Make sure that the dialog's window is in full screen
+    window?.decorView?.systemUiVisibility = FLAGS_FULLSCREEN
+
+    // Show the dialog while still in immersive mode
+    show()
+
+    // Set the dialog to focusable again
+    window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
 }
 
